91b56774aad7d02398725c19fb5d707b17cbdbc6
folajhimie <blackhemisphere12@gmail.com>
2023-09-10 01:22:50 +0100

    trying to fix the bug

--- src/controllers/Inventory/Category/categoryService.ts
+++ src/controllers/Inventory/Category/categoryService.ts
@@ -19,6 +19,7 @@ export class CategoryRepository implements ICategoryRepository {
                 name,
             });
 
+            
             // Save the category to the database
             await category.save();
 
